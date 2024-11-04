87fb5c9d9fd3c80b9cd5e73ccbb2bbd5367cad2a
alim <you@example.com>
2024-11-04 16:40:12 +0600

    texts

--- src/appPages/site/pages/school/SchoolPage.tsx
+++ src/appPages/site/pages/school/SchoolPage.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
-import School from './sections/aboutSchool/School'
-import Telegram from '../home/sections/telegram/Telegram'
-import Pack from './sections/pack/Pack'
-import Review from './sections/review/Review'
-import Master from './sections/master/Master'
-import Courses from './sections/courses/Courses'
-
+import React from "react";
+// import dynamic from 'next/dynamic'
+import School from "./sections/aboutSchool/School";
+import Telegram from "../home/sections/telegram/Telegram";
+import Pack from "./sections/pack/Pack";
+import Review from "./sections/review/Review";
+import Master from "./sections/master/Master";
+import Courses from "./sections/courses/Courses";
 
 const SchoolPage = () => {
-	return (
-		<>
-			<School />
-			<Courses />
-			<Master />
-			<Pack />
-			<Review />
-			<Telegram />
-		</>
-	)
-}
+  return (
+    <>
+      <School />
+      <Courses />
+      <Master />
+      <Pack />
+      <Review />
+      <Telegram />
+    </>
+  );
+};
 
-export default SchoolPage
+export default SchoolPage;
