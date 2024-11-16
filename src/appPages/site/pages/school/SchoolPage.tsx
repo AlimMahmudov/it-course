4a28d9744c68f72cddd800dd33d2b3464e5097ac
alim <you@example.com>
2024-11-16 13:26:43 +0600

    us adaptive

--- src/appPages/site/pages/school/SchoolPage.tsx
+++ src/appPages/site/pages/school/SchoolPage.tsx
@@ -1,26 +1,26 @@
-import Breadcrumbs from '@/shared/components/breadcrumbs/Breadcrumbs'
-import Telegram from '../home/sections/telegram/Telegram'
-import School from './sections/aboutSchool/School'
-import Master from './sections/master/Master'
-import Pack from './sections/pack/Pack'
-import Review from './sections/review/Review'
-import Courses from './sections/courses/Courses'
+import Breadcrumbs from "@/shared/components/breadcrumbs/Breadcrumbs";
+import Telegram from "../home/sections/telegram/Telegram";
+import School from "./sections/aboutSchool/School";
+import Master from "./sections/master/Master";
+import Pack from "./sections/pack/Pack";
+import Review from "./sections/review/Review";
+import Courses from "./sections/courses/Courses";
 const breadcrumbs = [
-	{ label: 'Главная', href: '/' },
-	{ label: 'О школе', href: '#this' }
-]
+  { label: "Главная", href: "/" },
+  { label: "О школе", href: "#this" },
+];
 const SchoolPage = () => {
-	return (
-		<>
-			<Breadcrumbs items={breadcrumbs} />
-			<School />
-			<Courses  />
-			<Master />
-			<Pack />
-			<Review />
-			<Telegram />
-		</>
-	)
-}
+  return (
+    <>
+      <Breadcrumbs items={breadcrumbs} />
+      <School />
+      <Courses />
+      <Master />
+      <Pack />
+      {/* <Review /> */}
+      {/* <Telegram /> */}
+    </>
+  );
+};
 
-export default SchoolPage
+export default SchoolPage;
